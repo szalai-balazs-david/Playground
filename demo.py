d1364d53e49876020cfbf971fa6f243f56623e73
Balazs Szalai <szalai.balazs.david@gmail.com>
2020-04-26 16:36:36 -0400

    end of Lesson2

--- demo.py
+++ demo.py
@@ -4,11 +4,29 @@ connection = psycopg2.connect(database="sample", user="postgres", password="6%R4
 
 cursor = connection.cursor()
 
+cursor.execute('DROP TABLE IF EXISTS table2;')
+
 cursor.execute('''CREATE TABLE table2 (id INTEGER PRIMARY KEY, completed BOOLEAN NOT NULL DEFAULT False);''')
 
-cursor.execute('INSERT INTO table2 (id, completed) VALUES (1, true);')
+cursor.execute('INSERT INTO table2 (id, completed) VALUES (%s, %s);', (1, True))
+
+SQL = 'INSERT INTO table2 (id, completed) VALUES (%(id)s, %(completed)s);'
+
+data = {
+  'id': 2,
+  'completed': True
+}
+cursor.execute(SQL, data)
 
 connection.commit()
+
+cursor.execute('SELECT * from table2;')
+result = cursor.fetchone()
+print(result)
+result = cursor.fetchone()
+print(result)
+result = cursor.fetchone()
+print(result)
 
 cursor.close()
 connection.close()
