09f5a6079ecb80a5ddd6f0ec91315f21f5485118
Balazs Szalai <szalai.balazs.david@gmail.com>
2020-04-30 17:57:55 -0400

    Lesson 7 Section 3

--- todoapp/app.py
+++ todoapp/app.py
@@ -20,7 +20,7 @@ class Todo(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 @app.route('/todos/create', methods=['POST'])
 def create():
@@ -28,7 +28,7 @@ def create():
     body = {}
     try:
         desc = request.get_json()['description']
-        todo = Todo(description2=desc)
+        todo = Todo(description=desc)
         db.session.add(todo)
         db.session.commit()
         body['description'] = todo.description
@@ -42,6 +42,19 @@ def create():
         abort(400)
     else:
         return jsonify(body)
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
